78acaf3b6843e14a592dfaf6ef274769930c81a2
Tony Su <peitaosu@163.com>
2018-10-02 19:33:26 +0800

    read from file

--- ini.py
+++ ini.py
@@ -3,3 +3,9 @@ import os, sys
 class INI():
     def __init__(self):
         self.file = None
+        self.ini = None
+    
+    def read_from_file(self, file_path):
+        self.file = file_path
+        with open(file_path) as in_file:
+            self.ini = in_file.readlines()
