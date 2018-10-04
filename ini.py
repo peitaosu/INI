fcad13897c15e42b60bafec58f7bbf9fb392a35b
Tony Su <peitaosu@163.com>
2018-10-04 22:11:53 +0800

    fix file path not updated issue

--- ini.py
+++ ini.py
@@ -2,14 +2,19 @@ import os, sys
 
 class INI():
     def __init__(self):
-        self.file = None
+        self.file_path = None
         self.ini = None
     
     def read_from_file(self, file_path):
-        self.file = file_path
-        with open(file_path) as in_file:
+        self.file_path = file_path
+        with open(self.file_path) as in_file:
             self.ini = in_file.readlines()
     
-    def write_to_file(self, file_path):
-        with open(file_path, "w") as out_file:
+    def write_to_file(self, file_path=None):
+        write_path = self.file_path
+        if not file_path:
+            write_path = file_path
+        with open(write_path, "w") as out_file:
             out_file.write(self.ini)
+    
+    
